11b0d58164f1b1ab5cbb9c06784bf1c83a5313cb
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-11-02 19:54:46 +0530

    test(bookmyshow): create test for `Movie` model

---
+++ 001-bookmyshow_tracker/test/models/movie_model_test.dart
@@ -0,0 +1,23 @@
+import 'package:bookmyshow_tracker/models/movie_model.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Movie model', () {
+    late Movie movie;
+
+    setUpAll(() {
+      movie = const Movie(title: 'title', url: 'url');
+    });
+
+    test('default initial values', () {
+      expect(movie.id, 'url');
+      expect(movie.isBookingAvailable, false);
+      expect(movie.trackingEnabled, true);
+    });
+
+    test('JSON serialization', () {
+      expect(movie, Movie.fromMap(movie.toMap()));
+      expect(movie, Movie.fromJson(movie.toJson()));
+    });
+  });
+}
