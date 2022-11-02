a64c1e552112ec5145e4760d82ac36683ce17b96
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-11-02 19:11:36 +0530

    refactor(bookmyshow): wrap app with `MoviesRepository`
    initialize localStorage in main().

--- 001-bookmyshow_tracker/lib/book_my_show_tracker.dart
+++ 001-bookmyshow_tracker/lib/book_my_show_tracker.dart
@@ -1,16 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:localstorage/localstorage.dart';
+import 'package:provider/provider.dart';
 
+import 'repository/movies_repository.dart';
 import 'screens/screens.dart';
 
 class BookMyShowTracker extends StatelessWidget {
-  const BookMyShowTracker({super.key});
+  const BookMyShowTracker({
+    super.key,
+    required this.localStorage,
+  });
+
+  final LocalStorage localStorage;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Bookmyshow Tracker',
-      darkTheme: ThemeData.dark(),
-      home: HomeScreen(),
+    return ChangeNotifierProvider<MoviesRepository>(
+      create: (context) => MoviesRepository(localStorage),
+      lazy: false,
+      child: MaterialApp(
+        title: 'Bookmyshow Tracker',
+        darkTheme: ThemeData.dark(),
+        home: HomeScreen(),
+      ),
     );
   }
 }
--- 001-bookmyshow_tracker/lib/main.dart
+++ 001-bookmyshow_tracker/lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:localstorage/localstorage.dart';
 
 import 'book_my_show_tracker.dart';
+import 'constants.dart';
 
 void main() {
-  runApp(const BookMyShowTracker());
+  final localStorage = LocalStorage(localStorageFileName);
+
+  runApp(BookMyShowTracker(localStorage: localStorage));
 }
