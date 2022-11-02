8bbb25d622a6ee12a7dd722bdddb5616e792d894
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-11-02 16:06:02 +0530

    feat(bookmyshow): add validation for `AddTrackerForm`
    create regex for Url.

---
+++ 001-bookmyshow_tracker/lib/constants.dart
@@ -0,0 +1,3 @@
+/// RegExp that matches movie homepage url on in.bookmyshow.com.
+const bookmyshowUrlRegexp =
+    r'^https:\/\/in\.bookmyshow\.com\/\w+[\w-]*\w\/movies\/\w+[\w-]*\w\/ET\d+\/?$';
--- 001-bookmyshow_tracker/lib/widgets/add_tracker_form.dart
+++ 001-bookmyshow_tracker/lib/widgets/add_tracker_form.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
 
+import '../constants.dart';
+import '../models/models.dart';
+
 /// A form to add new movie to tracking list.
-class AddTrackerForm extends StatelessWidget {
+class AddTrackerForm extends StatefulWidget {
   const AddTrackerForm({super.key});
+
+  @override
+  State<AddTrackerForm> createState() => _AddTrackerFormState();
+}
+
+class _AddTrackerFormState extends State<AddTrackerForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  // To store Form value
+  late String _title;
+  late String _url;
 
   @override
   Widget build(BuildContext context) {
     return Form(
+      key: _formKey,
       child: Padding(
         padding: const EdgeInsets.all(20),
         child: Column(
@@ -22,16 +37,34 @@ class AddTrackerForm extends StatelessWidget {
                 label: Text('Title'),
                 border: OutlineInputBorder(),
               ),
+              validator: (value) {
+                if (value?.trim().isEmpty ?? true) {
+                  return 'Please enter a title!';
+                }
+                return null;
+              },
+              onSaved: (value) => _title = value!.trim(),
             ),
             const SizedBox(height: 20),
             TextFormField(
               decoration: const InputDecoration(
-                label: Text('BookMyShow link'),
-                helperText: 'Link starts with:\n'
+                label: Text('BookMyShow url'),
+                helperText: 'Url starts with:\n'
                     '"https://in.bookmyshow.com/<place>/movies/"',
                 helperMaxLines: 5,
                 border: OutlineInputBorder(),
               ),
+              validator: (value) {
+                if (value?.trim().isEmpty ?? true) {
+                  return 'Please enter the movie Url!';
+                }
+                value = value!.trim();
+                if (!RegExp(bookmyshowUrlRegexp).hasMatch(value)) {
+                  return 'Please enter a valid Url!';
+                }
+                return null;
+              },
+              onSaved: (value) => _url = value!.trim(),
             ),
             const SizedBox(height: 20),
             Row(
@@ -42,12 +75,23 @@ class AddTrackerForm extends StatelessWidget {
                   child: const Text('CANCEL'),
                 ),
                 const SizedBox(width: 10),
-                ElevatedButton(onPressed: () {}, child: const Text('ADD')),
+                ElevatedButton(
+                  onPressed: _onSave,
+                  child: const Text('ADD'),
+                ),
               ],
             ),
           ],
         ),
       ),
     );
+  }
+
+  void _onSave() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      final movie = Movie(title: _title, url: _url);
+      print(movie);
+    }
   }
 }
