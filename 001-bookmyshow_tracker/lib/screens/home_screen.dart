6f2f293890f19d19fd48a7a552c056f930e4459d
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-11-02 10:00:35 +0530

    feat(bookmyshow): basic design
    add AppBar, ListView, FloatingActionButton.

--- 001-bookmyshow_tracker/lib/screens/home_screen.dart
+++ 001-bookmyshow_tracker/lib/screens/home_screen.dart
@@ -5,9 +5,29 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Home'),
+    return Scaffold(
+      appBar: AppBar(title: const Text('BookMyShow Tracker')),
+      body: ListView.builder(
+        itemCount: 5,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: const Text('Movie title'),
+            subtitle: const Text('Last checked at 10:30 AM'),
+            trailing: ClipOval(
+              child: ColoredBox(
+                color: index.isOdd ? Colors.green : Colors.redAccent,
+                child: Padding(
+                  padding: const EdgeInsets.all(5),
+                  child: Icon(index.isOdd ? Icons.done : Icons.close),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
       ),
     );
   }
