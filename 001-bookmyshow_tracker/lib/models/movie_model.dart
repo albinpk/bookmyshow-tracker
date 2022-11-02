d56ca7dba83b05f1948f39af2cafa174b96d5107
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-11-02 12:32:04 +0530

    refactor(bookmyshow): generate JSON serialization on `Move` model

--- 001-bookmyshow_tracker/lib/models/movie_model.dart
+++ 001-bookmyshow_tracker/lib/models/movie_model.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:equatable/equatable.dart';
 
 class Movie extends Equatable {
@@ -14,4 +16,24 @@ class Movie extends Equatable {
 
   @override
   List<Object> get props => [title, url];
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'title': title,
+      'url': url,
+    };
+  }
+
+  factory Movie.fromMap(Map<String, dynamic> map) {
+    return Movie(
+      title: map['title'] as String,
+      url: map['url'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Movie.fromJson(String source) {
+    return Movie.fromMap(json.decode(source) as Map<String, dynamic>);
+  }
 }
