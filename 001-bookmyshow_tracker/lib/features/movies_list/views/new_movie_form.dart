b4a640f2a2c20e0ae7fec87160e57d493cfbe190
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-11-21 20:44:24 +0530

    feat(bookmyshow): add info text in `NewMovieForm`
    fix overflow error.

--- 001-bookmyshow_tracker/lib/features/movies_list/views/new_movie_form.dart
+++ 001-bookmyshow_tracker/lib/features/movies_list/views/new_movie_form.dart
@@ -47,96 +47,110 @@ class _NewMovieFormState extends State<NewMovieForm> {
       key: _formKey,
       child: Padding(
         padding: const EdgeInsets.all(20),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            // Form title
-            Text(
-              'Add a movie to track',
-              style: Theme.of(context).textTheme.titleLarge,
-            ),
-            const SizedBox(height: 20),
-
-            // Title form field
-            TextFormField(
-              controller: _titleController,
-              decoration: const InputDecoration(
-                label: Text('Title'),
-                border: OutlineInputBorder(),
+        child: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              // Form title
+              Text(
+                'Add a movie to track',
+                style: Theme.of(context).textTheme.titleLarge,
               ),
-              textInputAction: TextInputAction.next,
-              validator: (title) {
-                if (title?.trim().isEmpty ?? true) {
-                  return 'Please enter a title!';
-                }
-                title = title!.trim();
-                if (context.read<MoviesListCubit>().contains(title: title)) {
-                  return 'Title already exist!';
-                }
-                return null;
-              },
-              onSaved: (title) => _title = title!.trim(),
-            ),
-            const SizedBox(height: 20),
+              const SizedBox(height: 20),
 
-            // Url form field
-            TextFormField(
-              initialValue: widget.url,
-              autofocus: true,
-              keyboardType: TextInputType.url,
-              decoration: const InputDecoration(
-                label: Text('BookMyShow url'),
-                helperText: 'Url starts with:\n'
-                    '"https://in.bookmyshow.com/<place>/movies/"',
-                helperMaxLines: 5,
-                border: OutlineInputBorder(),
+              // Title form field
+              TextFormField(
+                controller: _titleController,
+                decoration: const InputDecoration(
+                  label: Text('Title'),
+                  border: OutlineInputBorder(),
+                ),
+                textInputAction: TextInputAction.next,
+                validator: (title) {
+                  if (title?.trim().isEmpty ?? true) {
+                    return 'Please enter a title!';
+                  }
+                  title = title!.trim();
+                  if (context.read<MoviesListCubit>().contains(title: title)) {
+                    return 'Title already exist!';
+                  }
+                  return null;
+                },
+                onSaved: (title) => _title = title!.trim(),
               ),
-              onChanged: (url) {
-                if (_titleController.text.trim().isNotEmpty) return;
-                url = url.trim();
-                if (RegExp(bookmyshowUrlRegexp).hasMatch(url)) {
-                  _titleController.text = _getMovieNameFromLink(url);
-                }
-              },
-              onFieldSubmitted: (_) => _onSave(),
-              validator: (url) {
-                if (url?.trim().isEmpty ?? true) {
-                  return 'Please enter the movie Url!';
-                }
-                url = url!.trim();
-                if (!RegExp(bookmyshowUrlRegexp).hasMatch(url)) {
-                  return 'Please enter a valid Url!';
-                }
-                if (context.read<MoviesListCubit>().contains(url: url)) {
-                  return 'Url already exist!';
-                }
-                return null;
-              },
-              onSaved: (url) => _url = url!.trim(),
-            ),
-            const SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                TextButton(
-                  onPressed: () => Navigator.pop(context),
-                  child: const Text('CANCEL'),
+              const SizedBox(height: 20),
+
+              // Url form field
+              TextFormField(
+                initialValue: widget.url,
+                autofocus: true,
+                keyboardType: TextInputType.url,
+                decoration: const InputDecoration(
+                  label: Text('BookMyShow url'),
+                  helperText: 'Url starts with:\n'
+                      '"https://in.bookmyshow.com/<place>/movies/"',
+                  helperMaxLines: 5,
+                  border: OutlineInputBorder(),
                 ),
-                const SizedBox(width: 10),
-                ElevatedButton(
-                  onPressed: _isLoading ? null : _onSave,
-                  child: _isLoading
-                      ? const SizedBox.square(
-                          dimension: 20,
-                          child: CircularProgressIndicator(
-                            strokeWidth: 2,
-                          ),
-                        )
-                      : const Text('ADD'),
-                ),
-              ],
-            ),
-          ],
+                onChanged: (url) {
+                  if (_titleController.text.trim().isNotEmpty) return;
+                  url = url.trim();
+                  if (RegExp(bookmyshowUrlRegexp).hasMatch(url)) {
+                    _titleController.text = _getMovieNameFromLink(url);
+                  }
+                },
+                onFieldSubmitted: (_) => _onSave(),
+                validator: (url) {
+                  if (url?.trim().isEmpty ?? true) {
+                    return 'Please enter the movie Url!';
+                  }
+                  url = url!.trim();
+                  if (!RegExp(bookmyshowUrlRegexp).hasMatch(url)) {
+                    return 'Please enter a valid Url!';
+                  }
+                  if (context.read<MoviesListCubit>().contains(url: url)) {
+                    return 'Url already exist!';
+                  }
+                  return null;
+                },
+                onSaved: (url) => _url = url!.trim(),
+              ),
+
+              const SizedBox(height: 20),
+              Text(
+                'You can share a movie from the BookMyShow '
+                'app to avoid filling out this form.',
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyMedium!
+                    .copyWith(color: Colors.green),
+              ),
+              const SizedBox(height: 20),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () => Navigator.pop(context),
+                    child: const Text('CANCEL'),
+                  ),
+                  const SizedBox(width: 10),
+                  ElevatedButton(
+                    onPressed: _isLoading ? null : _onSave,
+                    child: _isLoading
+                        ? const SizedBox.square(
+                            dimension: 20,
+                            child: CircularProgressIndicator(
+                              strokeWidth: 2,
+                            ),
+                          )
+                        : const Text('ADD'),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
